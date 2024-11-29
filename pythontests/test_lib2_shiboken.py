0f3fdc0aa17af862a5da6c61def2c42dc9776dcf
Debao Zhang <hello@debao.me>
2024-11-30 01:34:01 +0800

    Add shiboken binding
    Change-Id: I01a5e8a7fe3ffe5da38f76bed9c16c876fe59a55

---
+++ pythontests/test_lib2_shiboken.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import mycommon
+
+sys.path.insert(0, mycommon.get_module_path())
+import lib2_shiboken
+
+# Test the function
+def test_lib2_add():
+    result = lib2_shiboken.add(1, 1)
+    print(f"lib2_shiboken.add(1,1) = {result}")
+    assert result == 2
+
+if __name__ == "__main__":
+    test_lib2_add()
