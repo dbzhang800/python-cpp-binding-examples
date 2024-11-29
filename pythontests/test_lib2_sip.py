783738333b8972049f3e5ffa1411079370e7c330
Debao Zhang <hello@debao.me>
2024-11-29 22:12:22 +0800

    test for sip binding works
    Change-Id: I522ae441e6bb7983fe14ca70fb946a41975f9442

--- pythontests/test_lib2_sip.py
+++ pythontests/test_lib2_sip.py
@@ -1,8 +1,26 @@
 import os
 import sys
+import glob
+import shutil
 import mycommon
 
-sys.path.insert(0, mycommon.get_module_path())
+mymodule_path = mycommon.get_module_path()
+
+def find_module_file():
+    """
+    Find the SIP generated module file.
+    SIP builds the module in a directory like:
+    'build/lib2_sip/build/lib.win-amd64-cpython-312/lib2_sip.*'
+    """
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    pattern = os.path.join(script_dir, "../bindings/lib2_sip/build/lib2_sip/build/lib.*/lib2_sip.*")
+    files = glob.glob(pattern)
+    return files[0] if files else None
+
+my_lib2_sip = find_module_file()
+shutil.copy2(my_lib2_sip, mymodule_path)
+
+sys.path.insert(0, mymodule_path)
 import lib2_sip
 
 # Test the function
